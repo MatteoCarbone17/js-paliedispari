9685c6cc3cb1ed963761073d1a5c567cf1b56d3c
MatteoCarbone17 <matteocarbone17@live.com>
2022-11-29 17:31:06 +0100

    push ( doesn't work to fix )

--- js/script.js
+++ js/script.js
@@ -1,12 +1,8 @@
 /*Palidroma
 Chiedere all’utente di inserire una parola:
 Creare una funzione per capire se la parola inserita è palindroma
-Pari e Dispari
-L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
-Generiamo un numero random (sempre da 1 a 5) per il computer attraverso una funzione.
-Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari attraverso un'altra funzione
-Dichiariamo chi ha vinto.
 */
+
 
 
 
@@ -27,6 +23,15 @@ function invertiParola(str){
 
 
 
+
+/*Pari e Dispari
+L’utente sceglie pari o dispari e inserisce un numero da 1 a 5.
+Generiamo un numero random (sempre da 1 a 5) per il computer attraverso una funzione.
+Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari attraverso un'altra funzione
+Dichiariamo chi ha vinto.
+*/
+
+
 // l'utente fa la sua scelta//
 
 let userChoice = prompt('scegli pari o dispari');
@@ -34,18 +39,46 @@ let userChoice = prompt('scegli pari o dispari');
 let userNumber = parseInt(prompt('inserisci un numero da 1  a 5'), 10);
 
 
+//funzione che assegna un numero random al computer//
+
 function getRandomNumber(numMin, numMax) {
-    const randomNumber = Math.floor( Math.random () * (numMax - numMin + 1) + numMin);
+    const randomNumber = Math.floor( Math.random () * (numMax - numMin + 1) + numMin); 
     return randomNumber;
 }
 
 
+//funzione che verifica se è pari //
+// ancora non usata//
 
 function isEven(number){
 
-    if (number % 2 == 0 ){
-        return true
+    if (number % 2 == 0){
+        return true ;
+    } else {
+        return false ;
     }
     
 
 }
+
+//funzione somma numeri per definire pari o dispari//
+let computerNumber = getRandomNumber(1,5);
+
+let sum = userNumber + computerNumber;
+
+function sumEvenAndOdd(sum) {
+
+    let winner = sum ;
+
+    if (sum % 2 === 0 ) {
+        console.log('ha vinto pari')
+    } else {
+        console.log('ha vinto dispari')
+    }
+    console.log(winner);
+    
+    return winner; 
+    
+}
+
+
